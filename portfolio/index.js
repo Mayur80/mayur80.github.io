dd7f1cc34570ca430406f6d842b1ae6f74cc44cb
Mayur Patel <you@example.com>
2022-09-29 20:02:41 +1000

    Added portfolio

---
+++ portfolio/index.js
@@ -0,0 +1,20 @@
+window.addEventListener("resize",getHW);
+
+const width = screen.width;
+const height = screen.height;
+const body = document.getElementsByTagName("BODY")[0];
+const submit = document.getElementById("submit");
+
+function getHW(){
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+   
+ //   if(width <= 1086){
+   //     body.style.fontSize = "50%" ;
+ //   }
+
+ //   else{
+  //      body.style.fontSize = "62.5%" ;
+  //  }
+}
+
